5aedc766615916838c3acddf53f404d8ae57e749
ekowdanful <paffadu-danful@st.ug.edu.gh>
2024-05-19 21:54:18 +0000

    Update arrayManipulation.js

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,6 +1,17 @@
 function processArray(numbers) {
     return numbers.map(num => num % 2 === 0 ? num * num : num * 3);
 }
+function formatArrayStrings(strings, processedNumbers) {
+    return strings.map((string, index) => {
+        if (processedNumbers[index] % 2 === 0) {
+            return string.toUpperCase();
+        } else {
+            return string.toLowerCase();
+        }
+    });
+}
+
 module.exports = {
-    processArray
+    processArray,
+    formatArrayStrings
 };
